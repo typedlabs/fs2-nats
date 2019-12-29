fa06efc854bd9fff5c3f76a76bddb06b34c73f7d
Joao Da Silva <joao@typedlabs.com>
2019-12-29 18:56:17 +0100

    travis-ci & sbt updates

--- .travis.sh
+++ .travis.sh
@@ -2,7 +2,7 @@
 
 sbt fmtCheck compile test &&
     if $(test ${TRAVIS_REPO_SLUG} == "typedlabs/fs2-nats" && test ${TRAVIS_PULL_REQUEST} == "false" && test "$TRAVIS_TAG" != ""); then
-      sbt +publishLocal
+      sbt +publish
     else
       exit 0 # skipping publish, it's regular build
     fi
